779a1c1641da005cd59ec21a79c361410f29ab9a
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-06-28 02:40:19 -0300

    Estudando sobre Serialização e Desserialização em arquivos JSON com .NET

---
+++ SerializacaoJson/Entitites/WeatherForecast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SerializacaoJson.Entitites
+{
+    public class WeatherForecast
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string Summary { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize(this);
+    }
+}
---
+++ SerializacaoJson/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using SerializacaoJson.Entitites;
+
+namespace SerializacaoJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<WeatherForecast> weatherForecast = new List<WeatherForecast>();
+
+            string jsonFileInput = @"D:\temp\input.json";
+            string jsonFileOutput = @"D:\temp\output.json";
+
+            using (var jsonReader = File.OpenText(jsonFileInput))
+            {
+                weatherForecast = JsonSerializer.Deserialize<IEnumerable<WeatherForecast>>(jsonReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+
+            using (var jsonWriter = File.OpenWrite(jsonFileOutput))
+            {
+                JsonSerializer.Serialize(new Utf8JsonWriter(jsonWriter,
+                    new JsonWriterOptions
+                    {
+                        Indented = true,
+                        SkipValidation = true
+                    }),
+                    weatherForecast
+                    );
+            }
+        }
+    }
+}
